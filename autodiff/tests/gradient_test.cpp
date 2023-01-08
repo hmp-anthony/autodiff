6ad9206a91c86c46ad5e0a8442e544620fe9267e
hmp-anthony <anthonygoddard962@gmail.com>
2023-01-08 16:49:40 +0000

    added another test

--- autodiff/tests/gradient_test.cpp
+++ autodiff/tests/gradient_test.cpp
@@ -58,8 +58,18 @@ TEST(functions, exp) {
     var a(2, 'a');
     auto exp_ = autodiff::functions::exp();
     auto c = exp_(a * a);
-
     auto C = gradient(c);
     ASSERT_NEAR(C['a'], 218.39, 0.1);
+
+    var b(3, 'b');
+    auto d = exp_(a * b);
+    auto D = gradient(d);
+    ASSERT_NEAR(D['a'], 1210.29, 0.1);
+    ASSERT_NEAR(D['b'], 806.85, 0.1);
+
+    auto e = exp_(a * b) + exp_(a * b);
+    auto E = gradient(e);
+    ASSERT_NEAR(E['a'], 2420.57, 0.1);
+    ASSERT_NEAR(E['b'], 1613.71, 0.1);
 }
 
